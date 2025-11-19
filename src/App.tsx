29976bf05279774fb5c03d18cf383c24bb0a7cdf
Christian Lugo <christianlugo1503@gmail.com>
2025-11-18 22:05:27 -0700

    Cambios en el layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { FaMoon, FaRegMoon, FaSun } from "react-icons/fa";
-import Table from "./components/table";
-import { useState } from "react";
-import { DynamicTable } from "./components/dynamic-table";
+import { useEffect, useState } from "react";
 
 const App = () => {
     const [isOn, setIsOn] = useState(true);
+    const [colorThead, setColorThead] = useState()
 
     const handleToggle = () => {
         setIsOn(!isOn)
@@ -27,19 +26,69 @@ const App = () => {
     ];
 
     return (
-        <>
-            <nav className="sticky top-0  dark:bg-slate-900 h-15 flex items-center justify-end px-20">
-                <a
-                    className="hover:scale-110 cursor-pointer transition ease-in-out dark:text-white text-slate-900"
-                    onClick={handleToggle}
-                >
-                    {isOn ? <FaSun size={25} /> : <FaMoon size={25} />}
-                </a>
-
-            </nav>
-            {/* <Table /> */}
-            <DynamicTable data={usuarios} />
-        </>
+        <div className="flex flex-row h-screen w-screen">
+            <div className="dark:bg-slate-800 w-80 mx-5 my-20 rounded-xl shadow-xl/20 p-5 flex flex-col gap-4 ">
+                <div className="flex flex-row justify-between items-center">
+                    <h1 className="dark:text-white text-slate-900 font-bold text-2xl">Propiedades</h1>
+                    <a
+                        className="hover:scale-110 cursor-pointer transition ease-in-out dark:text-white text-slate-900"
+                        onClick={handleToggle}
+                    >
+                        {isOn ? <FaSun size={25} /> : <FaMoon size={25} />}
+                    </a>
+                </div>
+                {/* body propierties */}
+                <div className="flex flex-col gap-4">
+                    <h1 className="dark:text-white text-lg font-semibold">Colores</h1>
+                    <div className="flex flex-row justify-center gap-5">
+                        <div className="flex flex-col justify-center items-center">
+                            <label className="dark:text-white" htmlFor="text">Texto</label>
+                            <input
+                                type="color"
+                                id="text"
+                                name="text"
+                                value={colorThead}
+                                className="cursor-pointer"
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <label className="dark:text-white" htmlFor="thead">Títulos</label>
+                            <input
+                                type="color"
+                                id="thead"
+                                name="thead"
+                                value={colorThead}
+                                className="cursor-pointer"
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <label className="dark:text-white" htmlFor="rows">Filas</label>
+                            <input
+                                type="color"
+                                id="rows"
+                                name="rows"
+                                value={colorThead}
+                                className="cursor-pointer"
+                            />
+                        </div>
+                    </div>
+                    <h1 className="dark:text-white text-lg font-semibold">Datos</h1>
+                    <div className="flex flex-col gap-4" >
+                        <textarea
+                            className="dark:bg-slate-700 dark:text-white rounded-lg w-full border border-gray-300 p-3"
+                            name="thead" rows={10}
+                            defaultValue="Títulos"
+                        />
+                        <textarea
+                            className="dark:bg-slate-700 dark:text-white rounded-lg w-full border border-gray-300 p-3"
+                            name="rows" rows={10}
+                            defaultValue="Filas"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className=" w-full h-full"></div>
+        </div>
     )
 }
 
